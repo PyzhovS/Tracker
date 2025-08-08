88e73876b6a2b683a5e78ae8302e49b0afbc947f
Sergey <pyzhovs.kz@yandex.kz>
2025-08-08 16:20:21 +0500

    удалил main

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -5,8 +5,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, AutoLayout!"
+        label.font =
+        .systemFont(ofSize: 20)
+        label.sizeToFit()
+        label.textAlignment =
+        .center
+        view.backgroundColor = .white
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
        
     }
+    
 
 
 }
