9f01421ea304200f31dadfde6df6faf40c68a3ce
Sergey <pyzhovs.kz@yandex.kz>
2025-08-08 16:10:01 +0500

    Начала нового проекта

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Sergey on 08.08.2025.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
